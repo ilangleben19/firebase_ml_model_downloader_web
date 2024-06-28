2dc5c42c8621886b742038129a1d0228cdc053f4
Ian Langleben <ilangleben19@gmail.com>
2024-06-28 16:32:12 -0400

    Create project with basic debugging

---
+++ lib/firebase_ml_model_downloader_web.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ml_model_downloader_platform_interface/firebase_ml_model_downloader_platform_interface.dart';
+
+class FirebaseMlModelDownloaderWeb extends FirebaseModelDownloaderPlatform {
+  static FirebaseMlModelDownloaderWeb get instance {
+    return FirebaseMlModelDownloaderWeb._();
+  }
+
+  FirebaseMlModelDownloaderWeb._() : super(appInstance: null);
+
+  FirebaseMlModelDownloaderWeb({required FirebaseApp app})
+      : super(appInstance: app);
+
+  @override
+  FirebaseModelDownloaderPlatform delegateFor({required FirebaseApp app}) {
+    return FirebaseMlModelDownloaderWeb(app: app);
+  }
+
+  /// Gets the downloaded model file based on download type and conditions.
+  @override
+  Future<FirebaseCustomModel> getModel(
+    String modelName,
+    FirebaseModelDownloadType downloadType,
+    FirebaseModelDownloadConditions conditions,
+  ) {
+    print("Web plugin working from getModel()! -Ian");
+    throw UnimplementedError('getModel() is not implemented');
+  }
+
+  /// Lists all models downloaded to device.
+  @override
+  Future<List<FirebaseCustomModel>> listDownloadedModels() {
+    print("Web plugin working from listDownloadedModels()! -Ian");
+    throw UnimplementedError('listDownloadedModels() is not implemented');
+  }
+
+  /// Deletes a locally downloaded model by name.
+  @override
+  Future<void> deleteDownloadedModel(String modelName) {
+    print("Web plugin working from deleteDownloadedModel()! -Ian");
+    throw UnimplementedError('deleteDownloadedModel() is not implemented');
+  }
+}
